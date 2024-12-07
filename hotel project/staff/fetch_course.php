516fd54ac7721021edbba4180492e787e14141ce
royality222 <148339308+royality222@users.noreply.github.com>
2024-12-07 01:44:06 +0100

    Delete hotel project directory

--- hotel project/staff/fetch_course.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'db.php';
-$id = $_GET['id'];
-$stmt = $conn->prepare("SELECT * FROM courses WHERE id = ?");
-$stmt->bind_param("i", $id);
-$stmt->execute();
-$result = $stmt->get_result();
-echo json_encode($result->fetch_assoc());
-?>
